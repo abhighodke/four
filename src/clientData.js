9daa77ace9ba0ea2346480fc6382118ebcf94bac
Abhishek Ghodke <abhighodke3@gmail.com>
2024-08-22 20:25:58 +0530

    Uploaded

---
+++ src/clientData.js
@@ -0,0 +1,37 @@
+// src/clientData.js
+const clientData = {
+    headerInitials: "KS", // Header Initials
+    heroTagline: "Kathy Shippen Realty", // Hero Tagline
+    agentName: "Kathy Shippen",
+    aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.", // AboutMe Short Description
+    aboutMeDetailed: `Licensed in 1994, I began my career selling "New Homes" in Summerlin and was awarded "Sales Agent of the Year" for 1995 in my category. Later moving into Residential Resale. I currently have my license with Realty ONE Group.
+
+My background is in the entertainment field, I danced professionally in some major production shows as well as in movies like "Staying Alive" with John Travolta, and MTV music videos and commercials.
+
+I assist the local artists and technical crews here in Las Vegas, along with their friends and families.
+
+Home buying can be stressful, but with my 25+ years of experience, I can make it a smooth transition. Let me help you find your dream home! Or... Sell your most valuable asset!`, // AboutMe Detailed Description
+    testimonials: [
+        { quote: "Kathy Shippen was amazing to work with, very helpful and professional, always prompt and offered great advice. As a first home buyer, Kathy made our experience pleasurable and stress-free. I highly recommend her to anyone buying.", author: "Phil" },
+        { quote: "Kathy is a sweet lady to work with as a realtor in finding our current home. She was always available to look at homes and made us feel really comfortable taking our time to make sure we found the one we wanted. She never swayed us one way or the other so we had our own timeline to find what was important to us. Thank you, Kathy!", author: "Denise" },
+        { quote: "Big shout out to my dear friend Kathy Shippen with Realty One Group... Great Job!!! Thank you SO much for your guidance, knowledge, and hard work in listing and selling our house and downsizing to our new townhome. You made it easy and seamless. Rick and I highly recommend you!!!!!", author: "Sharon & Rick" },
+        { quote: "I have no regret working with Kathy Shippen. I would work with her any time in the future, and I recommend her to anyone. She was proactive, knowledgeable, and very kind. I gave her some very strict and challenging deadlines to sell my home and she made it happen before time! She exceeded all of my expectations. She supported the negotiation with great insight based on an extended background in the real estate industry.", author: "Marc Antoine" },
+        { quote: "Kathy Shippen has handled 8 residential purchases for my husband & myself. She is very professional, honest, detail-oriented, plus knowledgeable in real estate rules and procedures. She is skilled at handling forms and responses in a prompt timely manner. I would definitely recommend Kathy for your next purchase as she always goes the 'extra mile' for her clients!", author: "Deb D." }
+    ], // Testimonials
+    footerTagline: "Your Trusted Partner in Luxury Real Estate", // Footer Tagline
+    email: "kathyshippen@outlook.com", // Email
+    address: "10750 W Charleston Blvd #180 Las Vegas, NV 89135-1043", // Address
+    phone: "(702)480-4954", // Phone Number
+    footerDescription: "Kathy Shippen Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.", // Footer Description
+    socialMediaLinks: {
+        facebook: "",
+        Instagram: "",
+        linkedin: "",
+    }, // Social Media Links
+    contactDetails: {
+        email: "kathyshippen@outlook.com",
+        phone: "(702)480-4954",
+    }, // Contact Details
+};
+
+export default clientData;
