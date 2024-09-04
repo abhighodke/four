895563282e65974e41bdfa82f1bd0914afe157d8
Abhishek Ghodke <abhighodke3@gmail.com>
2024-09-04 21:37:11 +0530

    client updated

--- src/clientData.js
+++ src/clientData.js
@@ -1,31 +1,31 @@
 // src/clientData.js
 const clientData = {
-    headerInitials: "CA",
-    heroTagline: "Crystal Akers Realty",
-    agentName: "Crystal Akers",
+    headerInitials: "CB",
+    heroTagline: "Courtney Betts Realty",
+    agentName: "Courtney Betts",
     aboutMeShort: "I specialize in finding perfect homes for buyers and sellers, including investors and veterans. My expertise and dedication to exceptional service guide you to the best decisions. Let's find your ideal home together.",
     aboutMeDetailed: `It is my passion to assist clients with reaching their goal of owning a home or selling their current home to find a new one. It is important to me that my clients find the ideal home to match their needs and their budget. Also, I have helped many investors find the perfect property to complete their investment portfolio. 
 
 With my knowledge and experience I provide my clients accurate and complete detailed information so they can make the best decisions. Also, with being certified to help our Veterans and their families in finding a home and utilizing their full VA eligibility. Allow me to assist you with your real estate needs.
 
 Customer Service excellence is our Passion!`,
-    testimonials: [{"quote": "Working with Crystal Akers was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Crystal Akers provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Crystal Akers were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Crystal Akers and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Crystal Akers is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
+    testimonials: [{"quote": "Working with Courtney Betts was a game-changer for us. Her deep knowledge of the New York market and personalized approach made finding our dream home effortless. Highly recommended for anyone seeking a top-notch real estate experience.", "author": "John and Emily R."}, {"quote": "Courtney Betts provided exceptional service from start to finish. The teams professionalism and attention to detail ensured that our property sale was smooth and successful. We could not have asked for a better partner in real estate.", "author": "Michael S."}, {"quote": "The dedication and expertise of Courtney Betts were evident throughout our home-buying journey. Her ability to understand our needs and navigate the competitive market made the process enjoyable and stress-free. We are thrilled with our new home!", "author": "Sarah and David L."}, {"quote": "Courtney Betts and Her team are truly outstanding. Their market knowledge, responsiveness, and client-focused approach set them apart. We have bought and sold multiple properties with them, and each experience has been exceptional.", "author": "Jessica and Mark T."}, {"quote": "If you are looking for a real estate agent who goes above and beyond, Courtney Betts is the one. Her dedication to finding the perfect property and ensuring a smooth transaction is unmatched. We are incredibly satisfied with the service we received.", "author": "Laura K."}],
     footerTagline: "Your Trusted Partner in Luxury Real Estate",
-    email: "",
-    address: "Prattville, Alabama, USA",
-    phone: "",
-    footerDescription: `Crystal Akers Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
+    email: "courtneybettsrealtor@gmail.com",
+    address: "175 Aliant Parkway, Alexander City, AL, 35010",
+    phone: "334-415-8328",
+    footerDescription: `Courtney Betts Realty is committed to delivering the highest standard of real estate services. Whether you're buying, selling, or investing in luxury properties, we offer unparalleled expertise and a personalized approach to meet your needs.`,
     socialMediaLinks: {
-        facebook: "https://www.facebook.com/csakers3/",
+        facebook: "https://www.facebook.com/cbettsrealtor",
         Instagram: "",
         linkedin: "",
     },
     contactDetails: {
-        email: "",
-        phone: "",
+        email: "courtneybettsrealtor@gmail.com",
+        phone: "334-415-8328",
     },
     website: "",
-    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/456873007_8831440243549982_1293178533016074158_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=BMkdRNnRA9gQ7kNvgHBB_XL&_nc_ht=scontent-sjc3-1.xx&oh=00_AYDCnTvrx5_nUI9cEqHDuqJSpGI8FL7DGj5f8Ps0oR4Skw&oe=66D7CE8F" // Agent image URL
+    dp: "https://scontent-sjc3-1.xx.fbcdn.net/v/t39.30808-6/432522153_905038408295466_7648572558940713883_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=D34xtMeEIYUQ7kNvgG0ScuE&_nc_ht=scontent-sjc3-1.xx&oh=00_AYDGEhFx4KuhZwU2WsGPZWkhKnOkq0_A0yawYQNhAlR1dA&oe=66DBBAB1" // Agent image URL
 };
 
 export default clientData;
